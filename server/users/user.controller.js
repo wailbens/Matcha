cc2615a5db2f46700482fd7c0f0319a78c53549c
RimLuna <bougsrim@gmail.com>
2021-09-02 15:33:36 +0200

    added: ugly validators, fixed: query promises

--- server/users/user.controller.js
+++ server/users/user.controller.js
@@ -1,33 +1,63 @@
 const { response } = require("express");
+const pool = require("../_helpers/db");
 const connection = require("../_helpers/db")
+const {validationResult} = require('express-validator');
+
+queryPromise1 = () => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT email, username, password FROM `users`', (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+        })
+    });
+}
+
+queryPromise2 = (username, email, password) => {
+    console.log(username, email, password);
+    return new Promise((resolve, reject) => {
+        pool.query(`INSERT INTO users (username, email, password) VALUES ("${username}", "${email}", "${password}")`, (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+        })
+    });
+}
 
 module.exports = {
     getUsers: async function(req,res,next) {
         try {
-            const rows = await connection.query("SELECT email, username FROM `users`",
-                (err, result, fields) => {
-                    if (result.length)
-                        res.json({
-                            users: result
-                        });
-                    res.json({
-                        message:"No user found"
-                    });
-                }
-            );  
-        } catch (err) {
-            next(err)
+            const results = await queryPromise1();
+            return res.status(200).send({
+                status: 'success',
+                data: results
+            });
+        } catch (error) {
+            return res.status(400).send({
+                status: 'error',
+                error: error.message
+            });
         }
     },
     register: async function(req, res, next) {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
         try {
-            const rows = await connection.query(`INSERT INTO users (username, email, password) VALUES (${req.username}, ${req.email}, ${req.password}))`,
-                (err, result, fields) => {
-                    return res.json({response: 'created'});
-                }
-            );
-        } catch (err) {
-            next(err)
+            // console.log(req.body.username)
+            const results = await queryPromise2(req.body.username, req.body.email, req.body.password);
+            return res.status(201).json({
+                status: 'created',
+            });
+        } catch (error) {
+            return res.status(400).send({
+                status: 'error',
+                error: error.message
+            });
         }
     }
 };
